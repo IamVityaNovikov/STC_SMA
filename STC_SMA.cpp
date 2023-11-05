3084a25c1fa1ea87d1e82b87e4d396c2c755f31a
Viktor Novikov <v.novikov@dr-cr.ru>
2023-11-05 19:05:01 +0300

    added SMA function

--- STC_SMA.cpp
+++ STC_SMA.cpp
@@ -1,7 +1,25 @@
 ﻿
 #include <iostream>
+#include <vector>
+
+//простая реализация
+void SMA(std::vector<float> data, int n) {
+    if (n > data.size()) {
+        return;
+    }
+    int t = n - 1;
+    while (t < data.size()) {
+        float sum = 0;
+        for (int i = t; i > t - n; i--) {
+            sum += data[i];
+        }
+        std::cout << sum / n << std::endl;
+        t++;
+    }
+}
 
 int main()
 {
-    std::cout << "test";
+    std::vector<float> data{ 1, 4, 3, 5, 2, 8, 11 };
+    SMA(data, 3);
 }
